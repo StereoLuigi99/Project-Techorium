5526c0d5c2a8dbb5461e100385bdb3c4260e88e8
StereoLuigi99 <cnadultswimsignonoff@gmail.com>
2024-06-19 16:32:46 +0300

    Updating repo to 1.45

--- chromium/popup.js
+++ chromium/popup.js
@@ -1,66 +1,149 @@
-window.onload = function() {
-    var body = document.body;
+window.onload = async function () {
+  var userThemeBox = document.getElementById("usertheme");
+  var savedTheme = await chrome.storage.local.get(["usertheme"]);
+  if (savedTheme !== null) {
+    userThemeBox.value = savedTheme["usertheme"];
+  }
+  var body = document.body;
+  var settingsContainer = document.getElementById("settingsContainer");
+  console.log("Hi");
+  var settings = [
+    {
+      id: "buttonTopColor",
+      imgSrcOn: "techorium-popup/toggle-on.png",
+      imgSrcOff: "techorium-popup/toggle-off.png",
+      storageKey: "buttonTopColorStatus",
+      label: "Logo bölümündeki mavi renk teması",
+      onEnable: function () {
+        console.log("Özellik açıldı!");
+      },
+      onDisable: function () {
+        console.log("Özellik kapatıldı!");
+      },
+    },
+    {
+      id: "buttonTopColor",
+      imgSrcOn: "techorium-popup/toggle-on.png",
+      imgSrcOff: "techorium-popup/toggle-off.png",
+      storageKey: "osBrowserStatus",
+<<<<<<< HEAD
+      label: "Herkesin OS/tarayıcı bilgisini gizle",
+=======
+      label: "Herkesin OS ve Tarayıcı Bilgisini gizle",
+>>>>>>> d4adedb33ebdcaee0f114b9c21abc9e54ff7516f
+      onEnable: function () {
+        console.log("Özellik açıldı!");
+      },
+      onDisable: function () {
+        console.log("Özellik kapatıldı!");
+      },
+    },
+    {
+      id: "buttonTopColor",
+      imgSrcOn: "techorium-popup/toggle-on.png",
+      imgSrcOff: "techorium-popup/toggle-off.png",
+      storageKey: "signatureStatus",
+      label: "Herkesin imzasını gizle",
+      onEnable: function () {
+        console.log("Özellik açıldı!");
+      },
+      onDisable: function () {
+        console.log("Özellik kapatıldı!");
+      },
+    },
+    {
+      id: "buttonTopColor",
+      imgSrcOn: "techorium-popup/toggle-on.png",
+      imgSrcOff: "techorium-popup/toggle-off.png",
+      storageKey: "changeIcons",
+<<<<<<< HEAD
+      label: "İşletim sistemi kategori ikonlarını iyileştir",
+=======
+      label: "Eklentinin bölüm simgelerini kullan",
+>>>>>>> d4adedb33ebdcaee0f114b9c21abc9e54ff7516f
+      onEnable: function () {
+        console.log("Özellik açıldı!");
+      },
+      onDisable: function () {
+        console.log("Özellik kapatıldı!");
+      },
+    },
+    // daha fazla ayar gelebilir. buradan sonra otherThings.js'e eklenecek.
+  ];
 
-    var buttonTopColor = document.getElementById("buttonTopColor");
-    var buttonTopColorIMG = buttonTopColor.getElementsByTagName("img")[0];
-
-    chrome.storage.local.get(["buttonTopColorStatus"]).then((local) => {
-        if (local.buttonTopColorStatus == "1") {
-            buttonTopColorIMG.setAttribute("src", "techorium-popup/toggle-on.png");
-        }
-        if (local.buttonTopColorStatus == "0") {
-            buttonTopColorIMG.setAttribute("src", "techorium-popup/toggle-off.png");
-        }
+  settings.forEach(function (setting) {
+    var settingContainer = document.createElement("div");
+    settingContainer.className = "settingContainer";
+    var button = document.createElement("button");
+    button.id = setting.id;
+    button.className = "settingButton";
+    var buttonImg = document.createElement("img");
+<<<<<<< HEAD
+    buttonImg.width = 30;
+=======
+    buttonImg.width = 20;
+>>>>>>> d4adedb33ebdcaee0f114b9c21abc9e54ff7516f
+    buttonImg.height = 20;
+    chrome.storage.local.get([setting.storageKey]).then(function (local) {
+      buttonImg.src =
+        local[setting.storageKey] == "1" ? setting.imgSrcOn : setting.imgSrcOff;
     });
-
-    buttonTopColor.addEventListener('click', function() {
-        var buttonTopColorIMGsrc = buttonTopColorIMG.getAttribute("src");
-        if (buttonTopColorIMGsrc == "techorium-popup/toggle-on.png") {
-            buttonTopColorIMG.setAttribute("src", "techorium-popup/toggle-off.png");
-            chrome.storage.local.set({ "buttonTopColorStatus" : "0" }).then(() => {
-                console.log("Özellik kapatıldı!");
-            });
-        }
-        if (buttonTopColorIMGsrc == "techorium-popup/toggle-off.png") {
-            buttonTopColorIMG.setAttribute("src", "techorium-popup/toggle-on.png");
-            chrome.storage.local.set({ "buttonTopColorStatus" : "1" }).then(() => {
-                console.log("Özellik açıldı!");
-            });
-        }
+    button.appendChild(buttonImg);
+    settingContainer.appendChild(button);
+    var label = document.createElement("p");
+    label.className = "descriptionText";
+    label.textContent = setting.label;
+    settingContainer.appendChild(label);
+    settingsContainer.appendChild(settingContainer);
+    button.addEventListener("click", function () {
+      chrome.storage.local.get([setting.storageKey]).then(function (local) {
+        var value = local[setting.storageKey] == "1" ? "0" : "1";
+        chrome.storage.local
+          .set({ [setting.storageKey]: value })
+          .then(function () {
+            buttonImg.src = value == "1" ? setting.imgSrcOn : setting.imgSrcOff;
+            if (value == "1") {
+              setting.onEnable();
+            } else {
+              setting.onDisable();
+            }
+          });
+      });
     });
+  });
 
-    var darkModeButton = document.getElementById("darkModeButton");
-    var darkModeButtonIMG = darkModeButton.getElementsByTagName("img")[0];
-    body.classList.add("lightMode");
+  userThemeBox?.addEventListener("change", function (e) {
+    chrome.storage.local.set({ ["usertheme"]: e.target.value });
+  });
 
-    chrome.storage.local.get(["darkModeStatus"]).then((local) => {
-        if (local.darkModeStatus == "1") {
-            darkModeButtonIMG.setAttribute("src", "techorium-popup/light-moon.png");
-            body.classList.remove("lightMode");
-            body.classList.add("darkMode");
-        }
-        if (local.darkModeStatus == "0") {
-            darkModeButtonIMG.setAttribute("src", "techorium-popup/dark-moon.png");
-        }
+<<<<<<< HEAD
+  var url = "https://visual917.github.io/versions/ahmet.txt"; // Yes, that's my name.
+  var res = await fetch(url);
+  var data = await res.text();
+  var currentVersion = "1.45";
+=======
+  var url = "https://visual917.github.io/versions/ahmet.txt";
+  var res = await fetch(url);
+  var data = await res.text();
+  var currentVersion = "1.42";
+>>>>>>> d4adedb33ebdcaee0f114b9c21abc9e54ff7516f
+  var newVersion = data.trim();
+  console.log("compar", currentVersion, newVersion);
+  console.log("compar", currentVersion.length, newVersion.length);
+  console.log("compar", typeof currentVersion, typeof newVersion);
+  console.log("compar", currentVersion != newVersion);
+  if (currentVersion != newVersion) {
+    var xd = document.getElementById("updatetext");
+    xd.style.cursor = "pointer";
+    xd.innerText = `Yeni sürüm ${newVersion} çıktı. Tıkla!`;
+    xd.addEventListener("click", function () {
+      window.open(
+        "https://github.com/StereoLuigi99/Project-Techorium/releases/latest",
+      );
     });
-
-    darkModeButton.addEventListener('click', function() {
-        var darkModeButtonIMGsrc = darkModeButtonIMG.getAttribute("src");
-        if (darkModeButtonIMGsrc == "techorium-popup/dark-moon.png") {
-            darkModeButtonIMG.setAttribute("src", "techorium-popup/light-moon.png");
-            body.classList.remove("lightMode");
-            body.classList.add("darkMode");
-            chrome.storage.local.set({ "darkModeStatus" : "1" }).then(() => {
-                console.log("Karanlık mod açıldı!");
-            });
-        }
-        if (darkModeButtonIMGsrc == "techorium-popup/light-moon.png") {
-            darkModeButtonIMG.setAttribute("src", "techorium-popup/dark-moon.png");
-            body.classList.remove("darkMode");
-            body.classList.add("lightMode");
-            chrome.storage.local.set({ "darkModeStatus" : "0" }).then(() => {
-                console.log("Karanlık mod kapatıldı!");
-            });
-        }
-    });
+  } else {
+    var stru = `Sürüm: ${currentVersion} | Güncelsiniz.`;
+    var xd = document.getElementById("updatetext");
+    xd.innerText = stru;
+  }
 };
--- firefox/js/otherThings.js
+++ firefox/js/otherThings.js
@@ -1,73 +1,106 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  browser.storage.local.get(["buttonTopColorStatus"]).then((local) => {
-    if (local.buttonTopColorStatus == "1") {
-      var darkTitleCSS = `.p-header {
-        background: url(${browser.runtime.getURL("techolay-title-dark.jpg")}) !important;
-      }
-      
-      .p-nav {
-        background: url(${browser.runtime.getURL("techolay-title-dark.jpg")}) !important;
-      }
-      
-      .p-nav-scroller .hScroller-action.hScroller-action--end {
-        background: linear-gradient(to right, rgba(30, 30, 30, 0) 0%, #003e8d 33%) !important;
-      }
-      
-      .p-nav-scroller .hScroller-action.hScroller-action--start {
-        background: linear-gradient(to right, #0052b9 66%, rgba(30, 30, 30, 0) 100%) !important;
-      }
-      
-      .p-nav-list .p-navEl:not(.is-selected) {
-        background: none !important;
-      }
-      
-      .p-nav-list .p-navEl:not(.is-selected):not(.is-menuOpen):hover, .p-nav-list .p-navEl:not(.is-selected):not(.is-menuOpen) .p-navEl-link:hover, .p-nav-list .p-navEl:not(.is-selected):not(.is-menuOpen) .p-navEl-splitTrigger:hover {
-        background: rgba(0, 0, 0, 0) !important;
-      }`;
-    
-      var darkTitleElem = document.createElement('style');
-      darkTitleElem.innerText = darkTitleCSS;
-    
-      var lightTitleCSS = `.p-header {
-        background: url(${browser.runtime.getURL("techolay-title-light.jpg")}) !important;
-      }
-      
-      .p-nav {
-        background: url(${browser.runtime.getURL("techolay-title-light.jpg")}) !important;
-      }
-      
-      .p-nav-scroller .hScroller-action.hScroller-action--end {
-        background: linear-gradient(to right, rgba(30, 30, 30, 0) 0%, #007eff 33%) !important;
-      }
-      
-      .p-nav-scroller .hScroller-action.hScroller-action--start {
-        background: linear-gradient(to right, #006edf 66%, rgba(30, 30, 30, 0) 100%) !important;
-      }
-      
-      .p-nav-list .p-navEl:not(.is-selected) {
-        background: none !important;
-      }
-      
-      .p-nav-list .p-navEl:not(.is-selected):not(.is-menuOpen):hover, .p-nav-list .p-navEl:not(.is-selected):not(.is-menuOpen) .p-navEl-link:hover, .p-nav-list .p-navEl:not(.is-selected):not(.is-menuOpen) .p-navEl-splitTrigger:hover {
-        background: rgba(0, 0, 0, 0) !important;
-      }`;
-    
-      var lightTitleElem = document.createElement('style');
-      lightTitleElem.innerText = lightTitleCSS;
-    
-      var pageWrapper = document.querySelector(".p-pageWrapper");
-      var pageWrapperCS = getComputedStyle(pageWrapper);
-    
-      if (pageWrapperCS.backgroundColor != "rgb(236, 236, 236)" && pageWrapperCS.backgroundColor != "rgb(0, 0, 0)") {
-          document.body.appendChild(darkTitleElem);
-      }
+window.onload = function () {
+  const settings = [
+    {
+      key: "buttonTopColorStatus",
+      styles: [
+        {
+          condition: color => color === "rgb(25, 25, 25)",
+          css: `
+                        .p-header {
+                            background: url(${browser.runtime.getURL(
+                              "techolay-title-dark.jpg",
+                            )}) !important;
+                        }
+                        .p-nav {
+                            background: url(${browser.runtime.getURL(
+                              "techolay-title-dark.jpg",
+                            )}) !important;
+                        }
+                        .p-nav-scroller .hScroller-action.hScroller-action--end {
+                            background: linear-gradient(to right, rgba(30, 30, 30, 0) 0%, #003e8d 33%) !important;
+                        }
+                        .p-nav-scroller .hScroller-action.hScroller-action--start {
+                            background: linear-gradient(to right, #0052b9 66%, rgba(30, 30, 30, 0) 100%) !important;
+                        }
+                        .p-nav-list .p-navEl:not(.is-selected) {
+                            background: none !important;
+                        }
+                        .p-nav-list .p-navEl:not(.is-selected):not(.is-menuOpen):hover, .p-nav-list .p-navEl:not(.is-selected):not(.is-menuOpen) .p-navEl-link:hover, .p-nav-list .p-navEl:not(.is-selected):not(.is-menuOpen) .p-navEl-splitTrigger:hover {
+                            background: rgba(0, 0, 0, 0) !important;
+                        }
+                    `,
+        },
+        {
+          condition: color => color === "rgb(236, 236, 236)",
+          css: `
+                        .p-header {
+                            background: url(${browser.runtime.getURL(
+                              "techolay-title-light.jpg",
+                            )}) !important;
+                        }
+                        .p-nav {
+                            background: url(${browser.runtime.getURL(
+                              "techolay-title-light.jpg",
+                            )}) !important;
+                        }
+                        .p-nav-scroller .hScroller-action.hScroller-action--end {
+                            background: linear-gradient(to right, rgba(30, 30, 30, 0) 0%, #007eff 33%) !important;
+                        }
+                        .p-nav-scroller .hScroller-action.hScroller-action--start {
+                            background: linear-gradient(to right, #006edf 66%, rgba(30, 30, 30, 0) 100%) !important;
+                        }
+                        .p-nav-list .p-navEl:not(.is-selected) {
+                            background: none !important;
+                        }
+                        .p-nav-list .p-navEl:not(.is-selected):not(.is-menuOpen):hover, .p-nav-list .p-navEl:not(.is-selected):not(.is-menuOpen) .p-navEl-link:hover, .p-nav-list .p-navEl:not(.is-selected):not(.is-menuOpen) .p-navEl-splitTrigger:hover {
+                            background: rgba(0, 0, 0, 0) !important;
+                        }
+                    `,
+        },
+      ],
+    },
+    {
+      key: "osBrowserStatus",
+      styles: [
+        {
+          css: `
+.iconspart {
+    display: none;
+}
+`,
+        },
+      ],
+    },
+    {
+      key: "signatureStatus",
+      styles: [
+        {
+          css: `
+.message-signature {
+    display: none;
+}
+`,
+        },
+      ],
+    },
+    // daha fazla ayar eklenebilir, ama önce popup.js'e eklenmeli.
+  ];
 
-      else if (pageWrapperCS.backgroundColor != "rgb(25, 25, 25)" && pageWrapperCS.backgroundColor != "rgb(0, 0, 0)") {
-          document.body.appendChild(lightTitleElem);
+  browser.storage.local.get(settings.map(s => s.key)).then(local => {
+    settings.forEach(setting => {
+      if (local[setting.key] === "1") {
+        const pageWrapper = document.querySelector(".p-pageWrapper");
+        const pageWrapperCS = getComputedStyle(pageWrapper);
+        const bgColor = pageWrapperCS.backgroundColor;
+
+        setting.styles.forEach(style => {
+          if (!style.condition || style.condition(bgColor)) {
+            const styleElem = document.createElement("style");
+            styleElem.innerText = style.css;
+            document.body.appendChild(styleElem);
+          }
+        });
       }
-    }
-    else {
-      return;
-    }
+    });
   });
-});
+};
